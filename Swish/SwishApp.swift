ddd2a24d41d0816329d1ba814a35da46f9d10bf8
Keys <70819367+keys-i@users.noreply.github.com>
2025-07-16 14:01:34 +1000

    Initial Commit

---
+++ Swish/SwishApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwishApp.swift
+//  Swish
+//
+//  Created by Radhesh Goel on 16/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwishTests/SwishTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwishTests.swift
+//  SwishTests
+//
+//  Created by Radhesh Goel on 16/7/2025.
+//
+
+import Testing
+@testable import Swish
+
+struct SwishTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
